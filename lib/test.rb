93a1d92ec5a5082dec0aceff6134533fc946a517
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-08-30 16:16:49 +0900

    add isprime

--- lib/test.rb
+++ lib/test.rb
@@ -1,4 +1,20 @@
 require 'mapleruby'
 
 Mapleruby.new('1+1').exec
-Mapleruby.new('nextprime(11)').exec
+
+class My_Maple
+  def nextprime(a)
+    p a = a.to_i
+    Mapleruby.new("nextprime(#{a})").exec
+  end
+  def isprime(a)
+    p a = a.to_i
+    Mapleruby.new("isprime(#{a})").exec
+  end
+end
+
+a = 1.0e10
+My_Maple.new.nextprime(a)
+[13,17,23,27].each{|a|
+p  My_Maple.new.isprime(a)
+}
