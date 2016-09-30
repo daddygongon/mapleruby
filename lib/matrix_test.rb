1527f64f0df3dc9c0c0b122b4df15f7cf8325042
Eri Murase <boa92316@kwansei.ac.jp>
2016-09-30 13:00:06 +0900

    inportmatrix様子見

---
+++ lib/matrix_test.rb
@@ -0,0 +1,6 @@
+require 'mapleruby'
+
+Mapleruby.new('Matrix([[1, 2], [3, 4]])').exec
+#data = Mapleruby.new('ImportMatrix("matrix.txt", delimiter=" ")').exec
+#Mapleruby.new('ExportMatrix("data.txt", data, delimiter=",")').exec
+
