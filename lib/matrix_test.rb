230ddb34645275ffcd107601ff1cfba2393aa482
Eri Murase <boa92316@kwansei.ac.jp>
2016-12-09 10:07:34 +0900

    exportmatrixが通らない

--- lib/matrix_test.rb
+++ lib/matrix_test.rb
@@ -1,12 +1,9 @@
 require './mapleruby'
 
-#Mapleruby.new('Matrix([[1, 2], [3, 4]])').exec
-
 p a = "matrix.txt"
 p b = " "
-#Mapleruby.new('ImportMatrix("matrix.txt", delimiter=" ")').exec
 
-RMaple.new.importmatrix("matrix.txt"," ")
+data = RMaple.new.importmatrix("matrix.txt"," ")
 
-#Mapleruby.new('ExportMatrix("data.txt", data, delimiter=",")').exec
+RMaple.new.exportmatrix("ExportData.csv",data,",")
 
