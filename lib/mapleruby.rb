9f02569c9183224f2539ba1e5854c8c4c287d619
Eri Murase <boa92316@kwansei.ac.jp>
2017-01-04 08:53:11 +0900

    行列関係のコマンド追加

--- lib/mapleruby.rb
+++ lib/mapleruby.rb
@@ -3,6 +3,7 @@ require 'systemu'
 require 'yaml'
 
 class RMaple
+  #整数論
   def nextprime(a)
     a = a.to_i
     p Mapleruby.new("nextprime(#{a})").exec_i
@@ -34,17 +35,49 @@ class RMaple
     a = a.to_i
     print(Mapleruby.new("ifactor(#{a})").exec_s)
   end
+
+  #行列
+  def matrix(a,b,c)
+    p a.to_i
+    p b.to_i
+    p c
+    puts text = "with(LinearAlgebra): matrix(#{a}, #{b}, #{c})"
+    p x = Mapleruby.new(text).exec_matrix(b)
+  end
   def importmatrix(a,b)
     p a
     p b
-    puts text = "ImportMatrix(\"#{a}\",delimiter=\"#{b}\")"
-    p Mapleruby.new(text).exec_s
+    text = "ImportMatrix(\"#{a}\",delimiter=\"#{b}\")"
+    p Mapleruby.new(text).exec_i
   end
+=begin
   def exportmatrix(a,b,c)
     p a
     p b
     p c
     puts text = "ExportMatrix(\"#{a}\",b,delimiter=\"#{c}\")"
+    p Mapleruby.new(text).exec_s
+  end
+=end
+  def matrixinverse(a)
+    p a
+    puts text = "with(LinearAlgebra): a:=convert(#{a},Matrix): MatrixInverse(a)"
+    p Mapleruby.new(text).exec_s
+  end
+  def determinant(a)
+    p a
+    puts text = "with(LinearAlgebra): a:=convert(#{a},Matrix): Determinant(a)"
+    p Mapleruby.new(text).exec_i
+  end
+  def transpose(a)
+    p a
+    puts text = "with(LinearAlgebra): a:=convert(#{a},Matrix): Transpose(a)"
+    p Mapleruby.new(text).exec_s
+  end
+  def eigenvectors(a)
+    p a
+    puts text = "with(LinearAlgebra): a:=convert(#{a},Matrix): Eigenvectors(a)"
+   # puts text = "with(LinearAlgebra): a:=convert(#{a},Matrix): evalf(Eigenvectors(a))"
     p Mapleruby.new(text).exec_s
   end
 end
@@ -69,8 +102,14 @@ class Mapleruby
  def exec_s
     result = exec
     return result
-  end
-  def exec
+ end
+ def exec_matrix(b)
+   x = exec.gsub(/[^\d]/, " ")
+   x1 = x.split(" ").map(&:to_i)
+   result = x1.each_slice(b).to_a
+   return result
+ end
+   def exec
     code0=<<EOS
 interface(quiet=true);
 writeto("./result.txt");
